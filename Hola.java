3164539a7e1aa92bf8700490f9fb8ba070f85815
raulmartel <raulmartelperez15@gmail.com>
2025-05-01 16:09:33 +0200

    Inferfaces creadas

---
+++ Hola.java
@@ -0,0 +1,24 @@
+package kaishdliad; // Add this line to declare the package
+
+import java.util.ArrayList;
+
+// Interfaces
+interface Curable {
+    void curar();
+}
+
+interface Volador {
+    void volar();
+}
+
+interface Magico {
+    void lanzarHechizo();
+}
+
+interface Defendible {
+    void defender();
+}
+
+interface Movilizable {
+    void moverse();
+}
