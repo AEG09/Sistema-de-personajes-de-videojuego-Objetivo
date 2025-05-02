e5507d5a10b72d44fc8d04d2d5e94cf02f1cfff3
AEG09 <alvaro.eugenio.g@gmail.com>
2025-05-02 19:52:28 +0100

    clases de personajes creadas

--- Hola.java
+++ Hola.java
@@ -73,3 +73,146 @@ abstract class PersonajeMagico extends Personaje {
         System.out.println(nombre + " medita para aumentar su poder mágico.");
     }
 }
+class Guerrero extends PersonajeFisico implements Defendible {
+    private int fuerza, armadura, escudo;
+
+    public Guerrero(String nombre, int nivel, int salud, int fuerza, int armadura, int escudo) {
+        super(nombre, nivel, salud);
+        this.fuerza = fuerza;
+        this.armadura = armadura;
+        this.escudo = escudo;
+    }
+
+    public void cargarAtaque() {
+        System.out.println(nombre + " carga un poderoso ataque físico.");
+    }
+
+    @Override
+    public void atacar() {
+        cargarAtaque();
+    }
+
+    @Override
+    public void defender() {
+        System.out.println(nombre + " se defiende con su escudo.");
+    }
+}
+
+class Mago extends PersonajeMagico implements Magico, Curable {
+    private int mana, sabiduria;
+
+    public Mago(String nombre, int nivel, int salud, int mana, int sabiduria) {
+        super(nombre, nivel, salud);
+        this.mana = mana;
+        this.sabiduria = sabiduria;
+    }
+
+    public void regenerarMana() {
+        System.out.println(nombre + " regenera su maná.");
+    }
+
+    @Override
+    public void atacar() {
+        lanzarHechizo();
+    }
+
+    @Override
+    public void lanzarHechizo() {
+        System.out.println(nombre + " lanza un hechizo de fuego.");
+    }
+
+    @Override
+    public void curar() {
+        System.out.println(nombre + " lanza un hechizo de curación.");
+    }
+}
+
+class Arquero extends PersonajeFisico implements Volador {
+    private int agilidad, numFlechas;
+
+    public Arquero(String nombre, int nivel, int salud, int agilidad, int numFlechas) {
+        super(nombre, nivel, salud);
+        this.agilidad = agilidad;
+        this.numFlechas = numFlechas;
+    }
+
+    public void dispararFlecha() {
+        System.out.println(nombre + " dispara una flecha con agilidad.");
+    }
+
+    public void reabastecerFlechas() {
+        System.out.println(nombre + " reabastece sus flechas.");
+    }
+
+    @Override
+    public void atacar() {
+        dispararFlecha();
+    }
+
+    @Override
+    public void volar() {
+        System.out.println(nombre + " usa un artefacto para volar.");
+    }
+}
+
+class Hechicero extends PersonajeMagico implements Magico, Defendible {
+    private int mana, concentracion;
+
+    public Hechicero(String nombre, int nivel, int salud, int mana, int concentracion) {
+        super(nombre, nivel, salud);
+        this.mana = mana;
+        this.concentracion = concentracion;
+    }
+
+    public void invocarEntidad() {
+        System.out.println(nombre + " invoca una entidad mágica.");
+    }
+
+    @Override
+    public void atacar() {
+        invocarEntidad();
+    }
+
+    @Override
+    public void lanzarHechizo() {
+        System.out.println(nombre + " lanza un hechizo ancestral.");
+    }
+
+    @Override
+    public void defender() {
+        System.out.println(nombre + " genera un escudo mágico para defenderse.");
+    }
+}
+
+class Asesino extends PersonajeFisico implements Movilizable, Curable {
+    private int sigilo, critico;
+
+    public Asesino(String nombre, int nivel, int salud, int sigilo, int critico) {
+        super(nombre, nivel, salud);
+        this.sigilo = sigilo;
+        this.critico = critico;
+    }
+
+    public void atacarPorLaEspalda() {
+        System.out.println(nombre + " ataca por la espalda con sigilo.");
+    }
+
+    public void ocultar() {
+        System.out.println(nombre + " se oculta en las sombras.");
+    }
+
+    @Override
+    public void atacar() {
+        atacarPorLaEspalda();
+    }
+
+    @Override
+    public void moverse() {
+        System.out.println(nombre + " se mueve sigilosamente.");
+    }
+
+    @Override
+    public void curar() {
+        System.out.println(nombre + " utiliza un ungüento para curarse.");
+    }
+}
